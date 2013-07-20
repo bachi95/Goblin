c9f69d1ec42df68598d9dac59d95ae62265ed264
Wayne Huang <bachi95tw@gmail.com>
2013-07-20 00:29:26 -0700

    rgba color with basic operator

---
+++ Source/GoblinColor.cpp
@@ -0,0 +1,13 @@
+#include "GoblinColor.h"
+
+namespace Goblin {
+    const Color Color::Red = Color(1.0f, 0.0f, 0.0f, 1.0f);
+    const Color Color::Green = Color(0.0f, 1.0f, 0.0f, 1.0f);
+    const Color Color::Blue  = Color(0.0f, 0.0f, 1.0f, 1.0f);
+    const Color Color::White = Color(1.0f, 1.0f, 1.0f, 1.0f);
+    const Color Color::Black  = Color(0.0f, 0.0f, 0.0f, 1.0f);
+    const Color Color::Yellow(1.0f, 1.0f, 0.0f, 1.0f);
+    const Color Color::Cyan(0.0f, 1.0f, 1.0f, 1.0f);
+    const Color Color::Magenta(1.0f, 0.0f, 1.0f, 1.0f);
+
+}
---
+++ Source/GoblinColor.h
@@ -0,0 +1,119 @@
+#ifndef GOBLIN_COLOR_H
+#define GOBLIN_COLOR_H
+
+#include <cassert>
+#include <cmath>
+#include <iostream>
+
+namespace Goblin {
+    class Color {
+    public:
+        static const Color Red;
+        static const Color Green;
+        static const Color Blue;
+        static const Color White;
+        static const Color Black;
+        static const Color Yellow;
+        static const Color Cyan;
+        static const Color Magenta;
+        float r, g, b, a;
+
+    public:
+        Color();
+        Color(float r, float g, float b, float a = 1.0f);
+        Color operator+(const Color& rhs) const;
+        Color& operator+=(const Color& rhs);
+        Color operator-(const Color& rhs) const;
+        Color& operator-=(const Color& rhs);
+        Color operator*(float s) const;
+        Color& operator*=(float s);
+        Color operator/(float s) const;
+        Color& operator/=(float s);
+        Color operator-() const;
+        bool operator==(const Color& rhs) const;
+        bool operator!=(const Color& rhs) const;
+        const float* ptr() const;
+    };
+
+    inline Color::Color() {}
+
+    inline Color::Color(float r, float g, float b, float a) : 
+        r(r), g(g), b(b), a(a) {}
+
+    inline Color Color::operator+(const Color& rhs) const {
+        return Color(r + rhs.r, g + rhs.g, b + rhs.b, a + rhs.a);
+    }
+
+    inline Color& Color::operator+=(const Color& rhs) {
+        r += rhs.r; 
+        g += rhs.g;
+        b += rhs.b;
+        a += rhs.a;
+        return *this;
+    }
+
+    inline Color Color::operator-(const Color& rhs) const {
+        return Color(r - rhs.r, g - rhs.g, b - rhs.b, a - rhs.a);
+    }
+
+    inline Color& Color::operator-=(const Color& rhs) {
+        r -= rhs.r;
+        g -= rhs.g;
+        b -= rhs.b;
+        a -= rhs.a;
+        return *this;
+    }
+
+    inline Color Color::operator*(float s) const {
+        return Color(r * s, g * s, b * s, a * s);
+    }
+
+    inline Color& Color::operator*=(float s) {
+        r *= s;
+        g *= s;
+        b *= s;
+        a *= s;
+        return *this;
+    }
+
+    inline Color Color::operator/(float s) const {
+        float inv = 1.0f / s;
+        return Color(r * inv, g * inv, b * inv, a * inv);
+    }
+
+    inline Color& Color::operator/=(float s) {
+        float inv = 1.0f / s;
+        r *= inv;
+        g *= inv;
+        b *= inv;
+        a *= inv;
+        return *this;
+    }
+
+    inline Color Color::operator-() const {
+        return Color(-r, -g, -b, -a);
+    }
+
+    inline bool Color::operator==(const Color& rhs) const {
+        return r == rhs.r && g == rhs.g && b == rhs.b && a == rhs.a;
+    }
+
+    inline bool Color::operator!=(const Color& rhs) const {
+        return !operator==(rhs);
+    }
+
+    inline const float* Color::ptr() const {
+        return &r;
+    }
+
+    inline Color operator*(float s, const Color& rhs) {
+        return rhs * s;
+    }
+
+    inline std::ostream& operator<<(std::ostream& os, const Color& c) {
+        os << "Color(" << c.r << ", " << c.g << ", " << c.b << 
+            ", " << c.a << ")";
+        return os;
+    }
+}
+#endif //GOBLIN_COLOR_H
